122bd7538662578fdcc53956e035493fb28a0209
gerasim <gerakhan64@gmail.com>
2021-11-17 17:02:24 +0300

    v1

---
+++ test_calc.py
@@ -0,0 +1,42 @@
+from calculator import Calculator
+
+
+def test_multiply_success():
+    cal = Calculator()
+    assert cal.multiply(3, 3) == 9
+
+
+def test_multiply_fail():
+    cal = Calculator()
+    assert cal.multiply(4, 3) == 9
+
+
+def test_adding_success():
+    cal = Calculator()
+    assert cal.adding(3, 3) == 6
+
+
+def test_adding_fail():
+    cal = Calculator()
+    assert cal.adding(4, 3) == 11
+
+
+def test_division_success():
+    cal = Calculator()
+    assert cal.division(3, 3) == 1
+
+
+def test_division_fail():
+    cal = Calculator()
+    assert cal.division(6, 3) == 5
+
+
+def test_subtraction_success():
+    cal = Calculator()
+    assert cal.subtraction(33, 3) == 30
+
+
+def test_subtraction_fail():
+    cal = Calculator()
+    assert cal.subtraction(4, 3) == 2
+
